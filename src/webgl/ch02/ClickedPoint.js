8470194cb641503b5d5e1a5205c5866d0c861cf4
jianghang546 <664019848@qq.com>
2025-03-31 00:40:56 +0800

    ch02

--- src/webgl/ch02/ClickedPoint.js
+++ src/webgl/ch02/ClickedPoint.js
@@ -39,7 +39,7 @@ function main() {
     // gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
     // var position = new Float32Array([0.0, 0.0, 0.0, 1.0]);
     // gl.vertexAttrib4fv(a_Position, position);
-    gl.vertexAttrib1f(a_PointSize, 6.0);
+    gl.vertexAttrib1f(a_PointSize, 10.0);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -54,16 +54,16 @@ function click(ev, gl, canvas, a_Position) {
     let y = ev.clientY;
     let rect = ev.target.getBoundingClientRect();
     console.log("rect.left: " + rect.left + " ,rect.top: " + rect.top);
-    x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2);
-    y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
-    g_points.push(x);
-    g_points.push(y);
+    x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
+    y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+    g_points.push([x, y]);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     let len = g_points.length;
-    for (let i = 0; i < len; i += 2) {
-        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+    for (let i = 0; i < len; i += 1) {
+        let xy = g_points[i];
+        gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
         gl.drawArrays(gl.POINTS, 0, 1);
     }
 }
